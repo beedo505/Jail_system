56cab11aba6edf49a0127de147e39cd16c61950f
beedo505 <banmark100@gmail.com>
2025-01-02 08:35:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def سجن(ctx, member: discord.Member = None, time_unit: str = "1d", *, r
             await ctx.message.reply("⚠️ Please mention the member you want to jail.")
             return
             
-        for mentioned in ctx.message.mentions:
+        for mentioned in ctx.member:
             if not isinstance(mentioned, discord.Member):
                 await ctx.send("❌ Please mention members only!")
                 return
