79fa4c6e7a4d9a5c6939a34babe73724173bd923
Badr <banmark100@gmail.com>
2025-01-25 06:31:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -143,9 +143,9 @@ async def on_ready():
             print(f"Created 'Prisoner' role in {guild.name}.")
             
             for channel in guild.channels:
-            await channel.set_permissions(prisoner_role, read_messages=False, send_messages=False)
-
-        print(f"Set 'Prisoner' role permissions to hide all channels in {guild.name}.")
+                await channel.set_permissions(prisoner_role, read_messages=False, send_messages=False)
+                
+            print(f"Set 'Prisoner' role permissions to hide all channels in {guild.name}.")
             
 
 @bot.event
