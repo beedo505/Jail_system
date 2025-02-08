034eb9c979a35c2198de43fc356000dbc3254bf0
Badr <banmark100@gmail.com>
2025-02-08 15:49:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -940,4 +940,4 @@ async def عفو(ctx, member: discord.Member = None):
     await ctx.message.reply(f"✅ {member.mention} has been pardoned!")
 
 
-bot.run(os.environ['B'])
+# bot.run(os.environ['B'])
