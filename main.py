abf3e840b1f7bd519e0748eda21f4bd728c75e69
Badr <banmark100@gmail.com>
2025-04-08 22:27:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ TOKEN = os.getenv('B')
 #     return response.text
 # print(get_current_ip())
 
-uri = "mongodb+srv://Bedo:bb35gFNqZyCo1rRQ@cluster0.zriaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://user_b:kzsF5rOLS61wHqYU@cluster0.zriaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(uri, tlsAllowInvalidCertificates=True)
 db = client["Prison"]
