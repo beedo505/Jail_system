d0de0ccb6e01980c0c064e384bc76498c32d0752
Badr <banmark100@gmail.com>
2025-04-13 20:39:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -827,7 +827,7 @@ async def سجن(ctx, member: discord.Member = None, duration: str = None, *, re
             continue
         previous_roles.append(role.id)
 
-    await member.edit(roles=[prisoner_role] + [r for r in member.roles if r.is_premium_subscriber()])
+    await member.edit(roles=[prisoner_role])
 
 
     collection.update_one(
