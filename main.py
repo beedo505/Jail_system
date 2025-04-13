01c48a08ab768f7a021306a802a429fc7a0a11ae
Badr <banmark100@gmail.com>
2025-04-13 21:58:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -147,8 +147,8 @@ async def on_ready():
     for guild in bot.guilds:
         guild_id = str(guild.id)
 
-    # Start checking for released prisoners every minute after the bot is ready
-    bot.loop.create_task(check_prisoners())
+        # Start checking for released prisoners every minute after the bot is ready
+        bot.loop.create_task(check_prisoners())
 
         # التحقق مما إذا كان السيرفر موجودًا في قاعدة البيانات، وإضافته إن لم يكن موجودًا
         server_data = guilds_collection.find_one({"guild_id": guild_id})
