5eb4a4ddea900e7c8c0a515ef23a7d78779eb105
Badr <banmark100@gmail.com>
2025-04-13 22:39:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -129,16 +129,17 @@ async def check_prisoners_loop():
 
             for prisoner in prisoners_data:
                 release_time = prisoner.get("release_time")
+                
                 if release_time:
                     if isinstance(release_time, str):
                         release_time = datetime.fromisoformat(release_time)
-                
-                if release_time <= now:
-                    member = guild.get_member(prisoner["user_id"])
-                    if member:
-                        await release_member(None, member, silent=True)
-                        collection.delete_one({"user_id": prisoner["user_id"], "guild_id": guild_id})
-        
+
+                    if release_time <= now:
+                        member = guild.get_member(prisoner["user_id"])
+                        if member:
+                            await release_member(None, member, silent=True)
+                            collection.delete_one({"user_id": prisoner["user_id"], "guild_id": guild_id})
+
         await asyncio.sleep(60)
 
 @bot.event
@@ -150,7 +151,7 @@ async def on_ready():
     await check_prisoners_loop()
 
     # Start checking for released prisoners every minute after the bot is ready
-    bot.loop.create_task(check_prisoners())
+    bot.loop.create_task(check_prisoners_loop())
 
     for guild in bot.guilds:
         guild_id = str(guild.id)
