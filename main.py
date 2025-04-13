e6ea8060f1a981398c535b13fa205c50e02bf9fa
Badr <banmark100@gmail.com>
2025-04-13 21:21:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -847,9 +847,10 @@ async def سجن(ctx, member: discord.Member = None, duration: str = None, *, re
     embed.add_field(name="السبب:", value=f"{reason}", inline=False)
     
     server_icon_url = guild.icon.url
-    embed.set_footer(text=f"Neral • {datetime.now().strftime('%-m/%-d/%Y %-I:%M %p')}", icon_url=server_icon_url)
-    await ctx.message.reply(embed=embed)
-
+    saudi_tz = ZoneInfo("Asia/Riyadh")
+    now_saudi = datetime.now(saudi_tz)
+    embed.set_footer(text=f"Neral • {now_saudi.strftime('%-m/%-d/%Y %-I:%M %p')}", icon_url=server_icon_url)    await ctx.message.reply(embed=embed)
+    
     await asyncio.sleep(delta.total_seconds())
     await release_member(ctx, member)
 
